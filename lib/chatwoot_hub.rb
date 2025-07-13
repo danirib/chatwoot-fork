36f52f854b9809c672326a7b98f6434e3e33b5e7
Nicolas Baldoino <120425784+nicolasbaldoino@users.noreply.github.com>
2025-07-13 18:24:26 -0300

    Update chatwoot_hub.rb

--- lib/chatwoot_hub.rb
+++ lib/chatwoot_hub.rb
@@ -23,7 +23,7 @@ class ChatwootHub
   end
 
   def self.pricing_plan_quantity
-    1000 # InstallationConfig.find_by(name: 'INSTALLATION_PRICING_PLAN_QUANTITY')&.value || 0
+    10000 # InstallationConfig.find_by(name: 'INSTALLATION_PRICING_PLAN_QUANTITY')&.value || 0
   end
 
   def self.support_config
